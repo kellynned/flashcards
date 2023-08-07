5a5ffc39dbbb9b12c06297b9130c80a4613931b9
Eric Camerer <ericcamerer@gmail.com>
2023-08-07 12:55:50 -0400

    Register page updates

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -7,7 +7,13 @@
       </div>
       <div class="form-input-group">
         <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
+        <input
+          type="text"
+          id="username"
+          v-model="user.username"
+          required
+          autofocus
+        />
       </div>
       <div class="form-input-group">
         <label for="password">Password</label>
@@ -15,44 +21,53 @@
       </div>
       <div class="form-input-group">
         <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
+        <input
+          type="password"
+          id="confirmPassword"
+          v-model="user.confirmPassword"
+          required
+        />
       </div>
       <button type="submit">Create Account</button>
-      <p><router-link :to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
+      <p>
+        <router-link :to="{ name: 'login' }" class = "login-link"
+          >Already have an account? Log in.</router-link
+        >
+      </p>
     </form>
   </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -60,14 +75,14 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
@@ -75,9 +90,56 @@ export default {
 
 <style scoped>
 .form-input-group {
+  display: flex;
+  flex-direction: column; /* Display label and input in a column layout */
   margin-bottom: 1rem;
 }
+
 label {
-  margin-right: 0.5rem;
+  text-align: left;
+  margin-bottom: 0.25rem; /* Add spacing between label and input */
+}
+
+input {
+  width: 100%;
+  padding: 0.5rem; /* Add some padding for better appearance */
+}
+
+#register {
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  background-color: #555b6e;
+  height: 95vh;
+  color: #89b0ae;
+  font-size: 1.5em;
+}
+
+h1 {
+  font-size: 4rem;
+  color: #faf9f9;
+}
+button {
+  display: flex;
+  border-radius: 4px;
+  background-color: #bee3db;
+  width: 75%;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  
+  font-weight: bold;
+}
+
+button:hover {
+  box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
+    0 17px 50px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 4px;
+  background-color: #89b0ae;
+}
+.login-link{
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
