96b764b579e7b1cb9d9b041694f1daf49cd83346
Luke <luke.d.brockman@gmail.com>
2023-08-08 10:39:36 -0400

    Database Day One

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS Flashcard_Deck, Deck, Flashcard, users Cascade;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +8,26 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+Create Table Flashcard (
+	flashcard_id serial,
+	deck_id serial,
+	subject varchar NOT NULL,
+	question
+	answer
+);
+
+Create Table Deck (
+	deck_id serial,
+	deckname
+	user_id
+);
+
+Create Table Flashcard_Deck (
+	flashcard_id int NOT NULL,
+	deck_id int NOT NULL,
+	sort
 );
 
 COMMIT TRANSACTION;
