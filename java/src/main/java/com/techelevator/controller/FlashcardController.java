5bf5690d8a930a62d602d8fbe54e10b0758ba919
Cody Krug <krug.cody@yahoo.com>
2023-08-10 13:35:15 -0400

    Added endpoint to delete flashcards

--- java/src/main/java/com/techelevator/controller/FlashcardController.java
+++ java/src/main/java/com/techelevator/controller/FlashcardController.java
@@ -39,4 +39,9 @@ public class FlashcardController {
         return jdbcFlashcardDao.findAll(creator);
     }
 
+    @RequestMapping(value = "/flashcard/{id}", method = RequestMethod.DELETE)
+    public void deleteFlashcard(@PathVariable int id, Principal principal){
+        jdbcFlashcardDao.deleteFlashcard(id);
+    }
+
 }
