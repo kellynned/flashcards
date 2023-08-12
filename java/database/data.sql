ff5abe98a08151fe4fa1755e530fbca68f60478e
Cody Krug <krug.cody@yahoo.com>
2023-08-12 17:16:27 -0400

    Fixed centering on flashcards

--- java/database/data.sql
+++ java/database/data.sql
@@ -5,18 +5,18 @@ INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpUL
 
 INSERT INTO flashcard (subject, question, answer, user_id)
 VALUES
-  ('math', 'What is the formula for the area of a circle?', 'A = πr^2', 1),
-  ('science', 'What is Newton''s first law of motion?', 'An object at rest will stay at rest, and an object in motion will stay in motion unless acted upon by an external force.', 1),
-  ('history', 'When did World War II end?', 'September 2, 1945', 2),
-  ('trivia', 'What is the largest mammal on Earth?', 'Blue whale', 2),
-  ('math', 'What is the quadratic formula?', 'x = (-b ± √(b^2 - 4ac)) / 2a', 1),
-  ('science', 'What is the chemical symbol for gold?', 'Au', 1),
-  ('math', 'What is the Pythagorean theorem?', 'In a right triangle, the square of the length of the hypotenuse is equal to the sum of the squares of the lengths of the other two sides.', 1),
-  ('science', 'What is photosynthesis?', 'The process by which green plants and some other organisms use sunlight to synthesize foods with the help of chlorophyll.', 1),
-  ('history', 'Who was the first President of the United States?', 'George Washington', 2),
-  ('trivia', 'What is the capital of Australia?', 'Canberra', 2),
-  ('math', 'What is the value of π (pi)?', 'Approximately 3.14159', 1),
-  ('trivia', 'What is the tallest mountain in the world?', 'Mount Everest', 2);
+  ('Math', 'What is the formula for the area of a circle?', 'A = πr^2', 1),
+  ('Math', 'What is the quadratic formula?', 'x = (-b ± √(b^2 - 4ac)) / 2a', 1),
+  ('Math', 'What is the Pythagorean theorem?', 'In a right triangle, the square of the length of the hypotenuse is equal to the sum of the squares of the lengths of the other two sides.', 1),
+  ('Math', 'What is the value of π (pi)?', 'Approximately 3.14159', 1),
+  ('Science', 'What is Newton''s first law of motion?', 'An object at rest will stay at rest, and an object in motion will stay in motion unless acted upon by an external force.', 1),
+  ('Science', 'What is the chemical symbol for gold?', 'Au', 1),
+  ('Science', 'What is photosynthesis?', 'The process by which green plants and some other organisms use sunlight to synthesize foods with the help of chlorophyll.', 1),
+  ('History', 'When did World War II end?', 'September 2, 1945', 2),
+  ('History', 'Who was the first President of the United States?', 'George Washington', 2),
+  ('Trivia', 'What is the largest mammal on Earth?', 'Blue whale', 2),
+  ('Trivia', 'What is the capital of Australia?', 'Canberra', 2),
+  ('Trivia', 'What is the tallest mountain in the world?', 'Mount Everest', 2);
 
  
   insert into deck (deckname, user_id)
